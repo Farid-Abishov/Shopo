282a334b4235cfb1bf73a68a0fcb5d1a15b1b2f5
Farid-Abishov <abisovfarid14@gmail.com>
2023-03-03 17:35:39 +0400

    first section

--- shopo/src/js/shopo.js
+++ shopo/src/js/shopo.js
@@ -1,4 +1,5 @@
 AOS.init();
+//modal
 const Modal =document.querySelector(".modal");
 const ModalBtn=document.querySelector(".modalclosebtn");
 window.addEventListener("load",()=>{
@@ -11,13 +12,21 @@ ModalBtn.addEventListener("click",()=>{
    Modal.style.display="none"
 })
 
-
+//language
 var UK=document.querySelector(".Uk");
 var USA=document.querySelector(".Usa");
 var BANGLA=document.querySelector(".Bangla");
 var UK_cntnt=document.querySelector(".ff");
 
 UK&&UK.addEventListener("click",()=>{
-   UK_cntnt.classList.toggle=("aa");
-   console.log("ok");
+   UK_cntnt.style.display="block";
+})
+
+
+//all category cntnt
+const Navcategory=document.querySelector(".nav_category");
+const Categry_cntnt=document.querySelector("category_cntnt");
+
+Navcategory&&Navcategory.addEventListener("click",()=>{
+   Categry_cntnt.classList.toggle=("actv_cate_cntnt");
 })
